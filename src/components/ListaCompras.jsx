a6cf61973829259c2fcb91ad9b8b36bad612fa10
JuanEndl <juanendl-_@hotmail.com>
2026-07-30 21:34:19 -0300

    agregado de tailwind

--- src/components/ListaCompras.jsx
+++ src/components/ListaCompras.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const ListaCompras = () => {
   return (
     <div>ListaCompras
-        <h3>lalala</h3>
+        <h3 className='bg-red-300'>lalala</h3>
     </div>
   )
 }
