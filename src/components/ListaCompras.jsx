64d586f0e3c013c7e228bf5d28692c073aa84fa8
JuanEndl <juanendl-_@hotmail.com>
2026-07-31 20:53:21 -0300

    arreglo del css

--- src/components/ListaCompras.jsx
+++ src/components/ListaCompras.jsx
@@ -12,7 +12,7 @@ export const ListaCompras = () => {
                 <div>asdsad</div>
                 <div>asdsad</div>
                 <div>asdsad</div>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full">
                     Agregar Producto
                 </button>
             </div>
