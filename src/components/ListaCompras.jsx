1a74d49eb5b118dca6737afe236435d359cf81e0
JuanEndl <juanendl-_@hotmail.com>
2026-07-30 23:49:08 -0300

    creando la visualizacion

--- src/components/ListaCompras.jsx
+++ src/components/ListaCompras.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
+
 export const ListaCompras = () => {
   return (
-    <div>ListaCompras
-        <h3 className='bg-red-300'>lalala</h3>
-    </div>
-  )
-}
+        <div className='contenedor'>
+            <h1  className='titulo'>Listas de compras </h1>
+            <div className='card'>lalala
+                <div>asdsad</div>
+                <div>asdsad</div>
+                <div>asdsad</div>
+                <div>asdsad</div>
+                <div>asdsad</div>
+                <div>asdsad</div>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                    Agregar Producto
+                </button>
+            </div>
+            
+        </div>
+  );
+};
