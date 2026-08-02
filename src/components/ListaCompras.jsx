d2e65d5555439b4b3772b6e79f121c1e391965bc
JuanEndl <juanendl-_@hotmail.com>
2026-08-01 23:01:53 -0300

    agregado de check

--- src/components/ListaCompras.jsx
+++ src/components/ListaCompras.jsx
@@ -2,24 +2,56 @@ import { useState } from "react";
 import { ModalProducto } from "./ModalProducto";
 
 export const ListaCompras = () => {
-
   const [modalAbierto, setModalAbierto] = useState(false);
 
+
+  /*productos seteados, si esta en false no esta marcado si esta en true se marca*/
   const [productos, setProductos] = useState([
-    "Leche",
-    "Huevos",
-    "Bebida",
+    {
+      id: crypto.randomUUID(),
+      nombre: "Leche",
+      comprado: false,
+    },
+    {
+      id: crypto.randomUUID(),
+      nombre: "Huevos",
+      comprado: false,
+    },
+    {
+      id: crypto.randomUUID(),
+      nombre: "Bebida",
+      comprado: false,
+    },
   ]);
 
+  /*Función que agrega un nuevo producto al estado*/
   const agregarProducto = (nombre) => {
     if (!nombre.trim()) return;
 
-    setProductos((anterior) => [
-      ...anterior,
-      nombre,
+    setProductos((productosAnteriores) => [
+      ...productosAnteriores,
+      {
+        id: crypto.randomUUID(),
+        nombre,
+        comprado: false,
+      },
     ]);
 
     setModalAbierto(false);
+  };
+
+  /* actualizar un elemento dentro de un array en React sin modificar el estado directamente */
+  const toggleSeleccion = (id) => {
+    setProductos((productosAnteriores) =>
+      productosAnteriores.map((producto) =>
+        producto.id === id
+          ? {
+              ...producto,
+              comprado: !producto.comprado,
+            }
+          : producto
+      )
+    );
   };
 
   return (
@@ -27,8 +59,22 @@ export const ListaCompras = () => {
       <h1 className="titulo">Lista de Compras</h1>
 
       <div className="card">
-        {productos.map((producto, index) => (
-          <p key={index}>{producto}</p>
+        {productos.map((producto) => (
+          <div key={producto.id} className="flex items-center justify-between w-full mb-3" >
+            <div className="flex items-center gap-3">
+              <input type="checkbox" checked={producto.comprado} onChange={() => toggleSeleccion(producto.id)} className="w-5 h-5 blue-green-600 cursor-pointer" />
+
+              <span
+                className={
+                  producto.comprado
+                    ? "line-through text-gray-400"
+                    : ""
+                }
+              >
+                {producto.nombre}
+              </span>
+            </div>
+          </div>
         ))}
 
         <button onClick={() => setModalAbierto(true)} className="mt-6 rounded-full bg-blue-500 px-8 py-3 font-bold text-white hover:bg-blue-700 transition" >
--- src/components/ModalProducto.jsx
+++ src/components/ModalProducto.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 
 export const ModalProducto = ({ abierto, cerrar, guardar }) => {
-
   const [nombre, setNombre] = useState("");
 
   if (!abierto) return null;
 
+  const agregar = () => {
+    guardar(nombre);
+    setNombre("");
+  };
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/50">
       <div className="w-96 rounded-xl bg-white p-6 shadow-xl">
-
         <h2 className="mb-4 text-2xl font-bold">
           Nuevo producto
         </h2>
@@ -17,16 +20,13 @@ export const ModalProducto = ({ abierto, cerrar, guardar }) => {
         <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} placeholder="Nombre del producto" className="mb-4 w-full rounded-lg border p-2" />
 
         <div className="flex justify-end gap-3">
-
           <button onClick={cerrar} className="rounded-lg bg-gray-400 px-5 py-2 text-white" >
             Cancelar
           </button>
 
-          <button
-            onClick={() => { guardar(nombre); setNombre(""); }} className="rounded-lg bg-green-600 px-5 py-2 text-white" >
+          <button onClick={agregar} className="rounded-lg bg-green-600 px-5 py-2 text-white" >
             Agregar
           </button>
-
         </div>
       </div>
     </div>
