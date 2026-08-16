ae2cee7b4306f52710fb9fb77b3f1257e80acfa9
juanendl <juanendl-_@hotmail.com>
2026-08-16 04:09:12 +0000

    modificacion de socket + agregado de Nginx location

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,18 @@
 import { io } from "socket.io-client";
 
-const url = import.meta.env.VITE_API_URL;
+export const socket = io("/", {
+  path: "/socket.io",
+  transports: ["websocket", "polling"]
+});
 
-export const socket = io(url);
+socket.on("connect", () => {
+  console.log("Socket conectado:", socket.id);
+});
+
+socket.on("connect_error", (error) => {
+  console.error("Socket error:", error);
+});
+
+socket.on("disconnect", (reason) => {
+  console.log("Socket desconectado:", reason);
+});
